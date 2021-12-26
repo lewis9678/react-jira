9187c143cd8fc8d833512bc125af30a85d168812
lewis9678 <qingao9678@163.com>
2021-12-26 14:29:51 +0800

    feat: 引入react-context

--- src/login/index.tsx
+++ src/login/index.tsx
@@ -1,17 +1,8 @@
 import { FormEvent } from "react";
+import { useAuth } from "context/auth-context";
 
 export const LoginScreen = () => {
-  const apiUrl = process.env.REACT_APP_API_URL;
-  const login = (data: { username: string; password: string }): void => {
-    fetch(`${apiUrl}/login?`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    }).then(async (response) => {
-      if (response.ok) {
-      }
-    });
-  };
+  const { login, user } = useAuth();
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const username = (event.currentTarget.elements[0] as HTMLInputElement)
@@ -25,13 +16,13 @@ export const LoginScreen = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">用户名</label>
-          <input type="text" id="username" />
+          <input type="text" id={"username"} />
         </div>
         <div>
           <label htmlFor="password">密码</label>
-          <input type="password" id="password" />
+          <input type="password" id={"password"} />
         </div>
-        <button type="submit">登录</button>
+        <button type={"submit"}>登录</button>
       </form>
     </div>
   );
